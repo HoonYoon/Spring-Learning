4b5982553e421f93e2507a4968abed9ab0401df9
Guillermo Ramirez <elguiche33@gmail.com>
2017-07-22 16:40:56 -0400

    init

---
+++ src/main/java/com/gilmo/factoring/FactoringApplication.java
@@ -0,0 +1,12 @@
+package com.gilmo.factoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoringApplication.class, args);
+	}
+}
